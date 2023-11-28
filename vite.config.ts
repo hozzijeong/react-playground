b2488783db34845ad4bdbd91c827ad577574cd32
hozzijeong <ghwls0905@naver.com>
2023-11-28 13:37:52 +0900

    chore: stylelint 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sass from 'sass';
+import stylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				implementation: sass,
+			},
+		},
+	},
+});
